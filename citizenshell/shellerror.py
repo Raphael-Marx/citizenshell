ed2f9027508f081ee65f00170d04f449a0081c4f
Raphael Marx <raphaelmarx@icloud.com>
2025-10-28 18:36:12 +0100

    Refactor ShellError constructor to use type hints and f-string formatting

--- citizenshell/shellerror.py
+++ citizenshell/shellerror.py
@@ -1,9 +1,9 @@
 class ShellError(RuntimeError):
 
-    def __init__(self, command, problem):
+    def __init__(self, command: str, problem: str):
         self._command = command        
         self._problem = problem
-        super(ShellError, self).__init__("'%s' terminated with %s" % (command, problem))
+        super().__init__(f"'{command}' terminated with {problem}")
 
     def command(self):
         return self._command

d32e569114eb1c771859132eebeda4622cb72213
Raphael-Marx <raphael.marx@outlook.de>
2025-10-29 15:41:22 +0100

    Refactor AbstractRemoteShell methods for consistency and type hinting

--- unishell/abstractremoteshell.py
+++ unishell/abstractremoteshell.py
@@ -8,19 +8,19 @@ class AbstractRemoteShell(AbstractShell):
 
     def __init__(self, target, *args, **kwargs):
         self._target = target
-        super(AbstractRemoteShell, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self._connected = False
 
-    def __repr__(self):
-        return "%s(id=%s,target=%s)" % (self.__class__.__name__, repr(self._id), repr(self._target))
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(id={repr(self._id)}, target={repr(self._target)})"
 
-    def is_connected(self):
+    def is_connected(self) -> bool:
         return self._connected
 
-    def connect(self):
+    def connect(self, timeout: float | None = None):
         if not self._connected:
             self.log_oob("connecting to '%s'..." % self._target)
-            self.do_connect()
+            self.do_connect(timeout=timeout)
             self._connected = True
 
     def disconnect(self):
@@ -29,7 +29,7 @@ class AbstractRemoteShell(AbstractShell):
             self.do_disconnect()
             self._connected = False
 
-    def do_connect(self):
+    def do_connect(self, timeout: float | None = None):
         raise NotImplementedError("this method should be implemented by subclass")
 
     def do_disconnect(self):

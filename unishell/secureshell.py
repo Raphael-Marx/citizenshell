3c8420334226e3f77c40e18c345df2f2afd9fb2f
Raphael-Marx <raphael.marx@outlook.de>
2025-10-29 14:44:43 +0100

    Add timeout handling to SecureShell commands and improve error reporting

--- unishell/secureshell.py
+++ unishell/secureshell.py
@@ -1,4 +1,5 @@
 import sys
+import socket
 if sys.version_info.major == 2:
     from warnings import filterwarnings
     filterwarnings("ignore", module=".*paramiko.*")
@@ -14,6 +15,11 @@ from scp import SCPClient
 from time import sleep
 from logging import CRITICAL
 
+
+class SecureShellException(BaseException):
+    pass
+
+
 class SecureShell(AbstractRemoteShell):
 
     def __init__(self, hostname, username, password=None, port=22,
@@ -26,29 +32,44 @@ class SecureShell(AbstractRemoteShell):
         self._password = password
         self.connect()
 
-    def do_connect(self):
+    def do_connect(self, timeout: float | None = None):
         self._client = SSHClient()
         self._client.load_system_host_keys()
         self._client.set_missing_host_key_policy(AutoAddPolicy())
-        self._client.connect(hostname=self._hostname, port=self._port, username=self._username, password=self._password)
+        self._client.connect(hostname=self._hostname, port=self._port, username=self._username, password=self._password, timeout=timeout)
         self._scp_client = SCPClient(self._client.get_transport())
 
     def do_disconnect(self):
         self._client.close()
 
-    def execute_command(self, command, env={}, wait=True, check_err=False, cwd=None):
+    def execute_command(self, command: str, env: dict = {}, wait: bool = True,
+                        check_err: bool = False, cwd=None, 
+                        timeout: float | None = None):
         for var, val in env.items():
-            command = "%s=%s; " % (var, val) + command
-        chan = self._client.get_transport().open_session()
-        chan.exec_command( (("cd \"%s\"; " % cwd) if cwd else "") + command)
-        queue = Queue()
-        StandardStreamReader(chan.makefile("r"), 1, queue)
-        StandardStreamReader(chan.makefile_stderr("r"), 2, queue)
-        def post_process_exit_code():
-            queue.put( (0, chan.recv_exit_status()) )
-            queue.put( (0, None) )
-        Thread(target=post_process_exit_code).start()
-        return ShellResult(self, command, queue, wait, check_err)
+            command = f"{var}={val}; {command}"
+
+        transport = self._client.get_transport()
+        if transport is not None:
+            chan = transport.open_session()
+            chan.settimeout(timeout=timeout)
+        else:
+            raise SecureShellException("Could not get transport from SSH client.")
+
+        try:
+            chan.exec_command( ((f"cd \"{cwd}\"; ") if cwd else "") + command)
+            queue = Queue()
+            StandardStreamReader(chan.makefile("r"), 1, queue)
+            StandardStreamReader(chan.makefile_stderr("r"), 2, queue)
+
+            def post_process_exit_code():
+                queue.put( (0, chan.recv_exit_status()) )
+                queue.put( (0, None) )
+            Thread(target=post_process_exit_code).start()
+
+            return ShellResult(self, command, queue, wait, check_err)
+
+        except socket.timeout:
+            raise SecureShellException("Command execution timed out.")
 
     def do_pull(self, local_path, remote_path):
         self._scp_client.get(remote_path, local_path)
